87c4f534e1c57e1c1052fd99a406a68f5700c1cb
robertscirulis <42386012+robertscirulis@users.noreply.github.com>
2018-09-03 19:48:56 +0300

    Day 1 created

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int veselsSkaitlis = 5;
+            double arKomatu = 4.5;
+            String simboluVirkne = "definejas sadi";
+            char simbols = 'g';
+            bool patiessVaiNe = true;
+
+            veselsSkaitlis = 6;
+            simboluVirkne = Console.ReadLine();
+
+            Console.ReadLine();
+        }
+    }
+}
