59ac223012e0ce8f56ddbcd9e8293d52fb47fa17
robertscirulis <42386012+robertscirulis@users.noreply.github.com>
2018-09-04 19:55:12 +0300

    Day 2 changes

--- Day1/Day1/Program.cs
+++ Day1/Day1/Program.cs
@@ -10,17 +10,58 @@ namespace Day1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Say something");
             int veselsSkaitlis = 5;
             double arKomatu = 4.5;
-            String simboluVirkne = "definejas sadi";
+            String simboluVirkne = "Hello?";
+            String simboluVirkne2 = "Is it me you're looking for?";
+            String simboluVirkne3 = "'Cause I wonder where you are";
             char simbols = 'g';
             bool patiessVaiNe = true;
 
             veselsSkaitlis = 6;
+
+            /*Console.WriteLine("Ievadiet skaitli");
             simboluVirkne = Console.ReadLine();
+            int jaunaisSkaitlis = Convert.ToInt16(simboluVirkne);
+
+            Console.WriteLine("Ievadiet skaitli 2");
+            simboluVirkne = Console.ReadLine();
+            int jaunaisSkaitlis2 = Convert.ToInt16(simboluVirkne);
+            Console.WriteLine(jaunaisSkaitlis + jaunaisSkaitlis2);*/
+
+
+            // <
+            // >
+            // ==
+            // !=
+            // <=
+            // >=
+
+            Console.WriteLine("Ievadiet skaitli");
+            simboluVirkne = Console.ReadLine();
+            int jaunaisSkaitlis = Convert.ToInt16(simboluVirkne);
+
+            if (jaunaisSkaitlis > 5 && jaunaisSkaitlis < 10)
+            {
+                Console.WriteLine("##");
+            }
+            else {
+                Console.WriteLine("#");
+            }
+
+
+            
 
             Console.ReadLine();
+            
+            /*Console.WriteLine(simboluVirkne);
+
+            Console.ReadLine();
+            Console.WriteLine(simboluVirkne2);
+            Console.ReadLine();
+            Console.WriteLine(simboluVirkne3);
+            Console.ReadLine();*/
         }
     }
 }
---
+++ day2/day2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day2
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            piemers();
+            //piemers();
+            Console.WriteLine("Ievadiet skaitli");
+            String simboluVirkne = Console.ReadLine();
+            int jaunaisSkaitlis = Convert.ToInt16(simboluVirkne);
+
+            Console.WriteLine("Ievadiet skaitli 2");
+            String simboluVirkne2 = Console.ReadLine();
+            int jaunaisSkaitlis2 = Convert.ToInt16(simboluVirkne2);
+
+            int summa = Saskaitisana(jaunaisSkaitlis, jaunaisSkaitlis2);
+            Console.WriteLine(summa);
+
+            Console.WriteLine(Saskaitisana(jaunaisSkaitlis, jaunaisSkaitlis2));
+
+            /*piemers2(3, 5);
+            
+            int skaitlis = piemers3();
+            Console.WriteLine(skaitlis);*/
+
+            Console.ReadLine();
+
+        }
+
+
+
+        static void piemers()
+        {
+            {
+
+                /*for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("Hello World!");
+                }
+
+                for (int i = 4; i > -1; i--)
+                {
+                    Console.WriteLine(i);
+                }
+
+                int j = 0;
+                while (j < 5)
+                {
+                    Console.WriteLine(j);
+                    j++;
+                }
+
+                j = 0;
+
+                do
+                {
+                    Console.WriteLine(j);
+                    j++;
+                } while (j < 5);
+
+                Console.ReadLine();*/
+
+               /* String star = "*";
+
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.WriteLine(star);
+                    star = star + '*';
+                }
+
+                Console.ReadLine();*/
+
+            }
+        }
+
+        static void piemers2(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        static int piemers3()
+        {
+            int rezultats = 6 + 8;
+
+            return rezultats;
+        }
+
+        static int Saskaitisana(int skaitlis1, int skaitlis2)
+        {
+            int rezultats = skaitlis1 + skaitlis2;
+            return rezultats;
+
+        }
+    }
+}
