99a782f37ac9b6102cea4e080cb1950b9154540e
robertscirulis <42386012+robertscirulis@users.noreply.github.com>
2018-09-13 14:25:46 +0300

    Update day 8

---
+++ Day 8/Day 8/PirmaisPiemers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_8
+{
+    class PirmaisPiemers
+    {
+
+        public void DarbibaArMasivu()
+        {
+
+            String[] masivs = new String[4];
+
+            String ievade = "";
+
+            while (ievade != "iziet")
+            {
+                for (int i = 0; i < masivs.Length; i++)
+                {
+                    Console.WriteLine("Ievadiet vertibu");
+                    ievade = Console.ReadLine();
+                    if (ievade == "iziet")
+                    {
+                        break;
+                    }
+                    masivs[i] = ievade;
+                    //Console.WriteLine(masivs[0] + masivs[1] + masivs[2] + masivs[3]);
+                    for (int j = 0; j < masivs.Length; j++)
+                    {
+                        Console.Write(masivs[j]);
+                    }
+                    Console.WriteLine();
+                }
+
+            }
+
+        }
+
+        public void Stripes()
+        {
+            String[] array = new string[4];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = "-";
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("-");
+            }
+
+        }
+    }
+}
---
+++ Day 8/Day 8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PirmaisPiemers pir = new PirmaisPiemers();
+
+            String izvele = "";
+
+            while (izvele != "iziet")
+            {
+                Console.WriteLine("Nospiediet 1, lai ievaditu masiva vertibas");
+                izvele = Console.ReadLine();
+                switch (izvele)
+                {
+                    case "1":
+                        pir.DarbibaArMasivu();
+                        break;
+                    case "2":
+                        pir.Stripes();
+                        break;
+                    case "0":
+                        break;
+                    default:
+                        Console.WriteLine("Nepareiza ievade");
+                        break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
